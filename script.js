85c0dfa696a660c7340c89cbe89fa6b9c301646c
Sarah Shokoor <sarahshokoor@gmail.com>
2023-02-06 22:11:27 -0800

    Second commit

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ let weather = {
         const { icon, description } = data.weather[0];
         const { temp, humidity } = data.main
         const { speed } = data.wind;
-        console.log(name, icon, description, temp, humidity, speed);
         document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".icon").src = 
             "http://openweathermap.org/img/wn/" + icon + ".png";
@@ -22,7 +21,23 @@ let weather = {
         document.querySelector(".temp").innerText = temp + "°F";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind Speed: " + speed + "km/h";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')"
     },
+    search: function() {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    }
 };
 
-// document.querySelector(".search button")
+document.querySelector(".search button")
+.addEventListener("click", function() {
+    weather.search();
+});
+
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+    if(event.key == "Enter") {
+        weather.search();
+    }
+});
+
+weather.fetchWeather("San Francisco");
